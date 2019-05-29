700ec0740ef55839da3e280d351d71528e034c75
Oriana Reina <orianna.nohemi@gmail.com>
2019-05-29 12:13:34 -0500

    Detectar colisiones. Hacer que los ladrillos desaparezcan cuando reciban un golpe.

--- gamedev-canvas-develop/js/main.js
+++ gamedev-canvas-develop/js/main.js
@@ -42,7 +42,7 @@ var bricks = [];
 for(c=0; c<brickColumnCount; c++) {
 	bricks[c] = [];
 	for(r=0; r<brickRowCount; r++) {
-		bricks[c][r] = { x : 0, y : 0};
+		bricks[c][r] = { x : 0, y : 0, status: 1 };
 	}
 }
 
@@ -70,7 +70,9 @@ function keyUpHandler(e) {
 function drawBall() {
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-	ctx.fillStyle = "#0095DD";
+	ctx.fillstyle = "#0033FF";
+	ctx.fillStroke = "#0033FF";
+	ctx.Stroke = "10";
 	ctx.fill();
 	ctx.closePath();
 }
@@ -86,6 +88,7 @@ function drawPaddle() {
 function drawBricks() {
 	for(c=0; c<brickColumnCount; c++) {
 		for(r=0; r<brickRowCount; r++) {
+			if (bricks[c][r].status == 1) {
 			var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
 			var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
 			bricks [c][r].x = brickX;
@@ -95,6 +98,21 @@ function drawBricks() {
 			ctx.fillStyle = "#0095DD";
 			ctx.fill();
 			ctx.closePath();
+			}
+		}
+	}
+}
+
+function collisionDetection() {
+	for(c=0; c<brickColumnCount; c++) {
+		for (r = 0; r <brickRowCount; r++) {
+			var b = bricks [c][r];
+			if(b.status == 1) {
+				if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+					dy = -dy;
+					b.status = 0;
+				}
+			}
 		}
 	}
 }
@@ -104,6 +122,7 @@ function draw() {
 	drawBricks();
 	drawBall();
 	drawPaddle();
+	collisionDetection();
 
 	if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
 		dx = -dx;
